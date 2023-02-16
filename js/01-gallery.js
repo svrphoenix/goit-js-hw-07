f812c4971c6d2b73fd2177c722a796569905951a
Serhii Rudenko <svr.phoenix@gmail.com>
2023-02-16 21:05:51 +0100

    Tasks 1,2 commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryItemsMarkup =
+  galleryItems
+    .map(
+      ({preview, original, description}) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join('');
+galleryRef.innerHTML = galleryItemsMarkup;
+
+galleryRef.addEventListener('click', onGalleryClick);
+
+  function onGalleryClick(event) {
+    event.preventDefault();
+
+    const { target } = event;
+    if (target.nodeName !== 'IMG') {
+      return;
+    }
+    const { source } = target.dataset;
+
+    const modal = basicLightbox.create(`<img src="${source}" width="800" height="600">`,
+    {
+      onShow: () => {
+        document.addEventListener('keydown', onCloseModal);
+      },
+      onClose: () => {
+        document.removeEventListener('keydown', onCloseModal);
+      },
+      });
+    
+    modal.show();
+
+    function onCloseModal(event) {
+      if (event.code === 'Escape') {
+        modal.close();
+      }
+    }
+  }
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryItemsMarkup = galleryItems
+  .map(
+    ({preview, original, description}) =>
+      `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`
+  )
+  .join('');
+galleryRef.innerHTML = galleryItemsMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
